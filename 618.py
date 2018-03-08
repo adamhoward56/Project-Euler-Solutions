15fb5c06a6b75c10fd0e2984b0f0eb01fc92106f
Adam Howard <adamhoward56@gmail.com>
2018-03-08 15:30:53 -0500

    Initial commit

---
+++ 618.py
@@ -0,0 +1,50 @@
+fib = []
+#factors = []
+prime = []
+
+# Generate given number of Fibonacci numbers
+def generateFib():
+	fib_count = int(input("Enter the number of fibonacci numbers to include: "))
+	for i in range (0,fib_count):
+		s = 0
+		if (len(fib) == 0):
+			fib.append(1)
+		fib.append(fib[i-1] + fib[i])
+
+def generatePrime():
+	for i in range(2,522):
+		p = True
+		for x in range(2, i-1):
+			if i % x == 0:
+				p = False
+
+		if p == True:
+			prime.append(i)
+
+def getPrimeFactorSum(x):
+	f = []
+	num = x
+	i = 0
+	while i < len(prime):
+		if num == prime[i]:
+			f.append(prime[i])
+		if num % prime[i] == 0 and prime[i] < num:
+			f.append(prime[i])
+			num = num/prime[i]
+		else:
+			i = i + 1
+	#factors.append(f)
+
+	s = 0
+	#for i in # Finish this line 
+
+# Start the program
+def init():
+	generateFib()
+	print("\nFibonacci Numbers: " + str(fib))
+	generatePrime()
+	print("\nPrime Numbers: " + str(prime))
+
+	print()
+
+init()
